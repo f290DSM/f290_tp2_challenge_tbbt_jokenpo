a712535361b3492b8091a5e6339092df45e615cb
Esdras B Silva <bzr4@icloud.com>
2023-03-03 16:05:33 -0300

    versao-4
    - Implementação de Cliente e Contexto;
    - Falta implemntar os demais Algorimos [Tesoura, Pedra, Lagarto e Spock];

--- src/main/java/br/com/fatecararas/Main.java
+++ src/main/java/br/com/fatecararas/Main.java
@@ -1,5 +1,6 @@
 package br.com.fatecararas;
 
+import br.com.fatecararas.domain.Algoritmo;
 import br.com.fatecararas.domain.Jokenpo;
 import br.com.fatecararas.domain.Papel;
 import br.com.fatecararas.domain.Tipo;
@@ -8,27 +9,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         var jokenpo = new Jokenpo();
 
         System.out.println("Escolha uma das opções [ 1-PAPEL, 2-TESOURA, 3-PEDRA, LAGARTO-LAGARTO, 5-SPOCK ]\n");
-//        int jogada = in.nextInt();
+        int jogada = in.nextInt();
         int computador = new Random().nextInt(5) + 1;
         System.out.println("O COMPUTADOR escolheu: " + Tipo.getInstance(computador));
 
         //TODO: Implementar regra para criação do tipo om base na classe Algoritmo
-        var papel = new Papel();
+        var algoritmo = getAlgoritmo(jogada);
 
-        jokenpo.setAlgoritmo(papel);
+        jokenpo.setAlgoritmo(algoritmo);
         Tipo tipoJogadaComputador = Tipo.getInstance(computador);
 
         jokenpo.jogar(tipoJogadaComputador);
 
         in.close();
 
+    }
+    private static Algoritmo getAlgoritmo(Integer pId) {
+        switch (pId) {
+            case 1 -> new Papel();
+            case 2 -> new Object(); //TODO: Implementar a classe Tesoura
+            case 3 -> new Object(); //TODO: Implementar a classe Pedra
+            case 4 -> new Object(); //TODO: Implementar a classe Lagarto
+            case 5 -> new Object(); //TODO: Implementar a classe Spock
+            default -> throw new IllegalStateException("Unexpected value: " + pId);
+        }
+        return null;
     }
 }
